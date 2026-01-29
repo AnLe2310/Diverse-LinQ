e19934c0c5ce6dff630adcda50ccd2ee3dbb96f7
AnThanhLe <an.thanhle@infodation.vn>
2026-01-30 00:12:33 +0700

    config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/Diverse-LinQ/',
   server: {
     host: '::',
     port: 8080,
